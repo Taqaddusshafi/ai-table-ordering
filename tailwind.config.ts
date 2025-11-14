e8984fca7015708e605b504771b98fa22ed35893
Taqaddus <taqaddusshafi9086@gmail.com>
2025-11-14 12:02:40 +0530

    inputboxissuefixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: false, // 🔥 disable dark mode completely (strict one theme)
+  darkMode: 'media', // or false to completely disable
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
